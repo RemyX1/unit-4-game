b95d9480fdcc868ad784cd55ad0b90f96842df7a
Remy <remington49@rocketmail.com>
2019-01-21 20:59:30 -0500

    Near Done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,36 +9,90 @@ var losses = 0;
 //Point Counter
 var points = 0;
 
+var gamefinished = false;
+
 $(document).ready(function () {
 
     // For loop populates #Gems div with images and assigns them values.
+    function GameStart() {
 
+        
     for (i = 0; i < 4; i++) {
 
-        var Pic = $(document.createElement('img'))
-        Pic.attr("src", "./assets/images/Gem1.png")
-        Pic.attr("id", i)
-        Pic.attr("value", Math.floor(Math.random()*50))
-        Pic.addClass("col-lg-3")
-        $("#Gems").append(Pic)
+        var pic = $(document.createElement('img'))
+        pic.attr("src", "./assets/images/Gem1.png")
+        pic.attr("id", i)
+        pic.attr("value", Math.floor(Math.random()*5))
+        pic.addClass("col-lg-3")
+        $("#Gems").append(pic)
         console.log("This is the", i, "Pic")
-        console.log(Pic)
-        
+        goal = Math.floor(Math.random()*20)
+        console.log(pic.attr("value"))
+         
+         };
 
+    $("#goal").html(goal)
     };
+
+       
+
     
+
+
+    GameStart()
     
-  $("#points").append(points)
+    $("#points").html(points)
+    $("#goal").html(goal)
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+
   
-    //Click event that allows for gameplay.
+      
+        
+    
+
 
     $("img").on("click", function () {
-        console.log("HI")
-        console.log(this.val)
+        var pointvalue = $(this).attr("value");
+        pointvalue = parseInt(pointvalue)
+        points = points + pointvalue;
+        $("#points").html(points)
+   
+        if (points === goal) {
 
+            wins = wins + 1;
+            $("#wins").html(wins)
+            points = 0;
+            $("#points").html(points)
+            $("#Gems").html("")
+            GameStart()
+    
+        }
+    
+        else if (points > goal) {
+            losses = losses + 1;
+            $("#losses").html(losses)
+            points = 0;
+            $("#points").html(points)
+            $("#Gems").html("")
+            GameStart()
+    
+        }
+    
+    
+    
+    
+    
+    
+    
+    
     });
+        
+       
 
-    
-    
-
+ 
 });
+    
+
+    
+
